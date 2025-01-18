1663c2bcd78d341c54faee3a1c74bcc491cc9ccb
Grybos Kamil <grybos.kamil@gmail.com>
2025-01-18 23:03:03 +0100

    Update FirstScene.

--- src/rtf/FirstScene.tsx
+++ src/rtf/FirstScene.tsx
@@ -3,8 +3,10 @@ import { Canvas } from "@react-three/fiber";
 export function FirstScene() {
   return (
     <Canvas>
+      <ambientLight intensity={0.1} />
+      <directionalLight color="red" position={[0, 0, 5]} />
       <mesh>
-        <boxGeometry />
+        <boxGeometry args={[2, 2, 2]} />
         <meshStandardMaterial />
       </mesh>
     </Canvas>
