ae9252b3b844e09e9e6fd289ff92474701a3d12f
Grybos Kamil <grybos.kamil@gmail.com>
2025-01-21 22:32:08 +0100

    Add Painting rotation.

--- src/rtf/gallery/Paiting.tsx
+++ src/rtf/gallery/Paiting.tsx
@@ -1,15 +1,30 @@
-import { useLoader } from "@react-three/fiber";
-import { Suspense, useEffect, useState } from "react";
+import { useFrame, useLoader } from "@react-three/fiber";
+import { Suspense, useEffect, useRef, useState } from "react";
 import * as THREE from "three";
 
 export type PaintingProps = {
   url: string;
   initialPosition?: THREE.Vector3;
+  initialRotation?: THREE.Euler;
 };
 
-export function Painting({ url, initialPosition }: PaintingProps) {
+export function Painting({
+  url,
+  initialPosition,
+  initialRotation,
+}: PaintingProps) {
   const texture = useLoader(THREE.TextureLoader, url);
   const [dimensions, setDimensions] = useState({ width: 1, height: 1 });
+
+  const meshRef = useRef<THREE.Mesh>(null);
+
+  const [rotationDirection] = useState<THREE.Euler>(
+    new THREE.Euler(
+      getRandomNumberInRange(-0.005, 0.005),
+      getRandomNumberInRange(-0.005, 0.005),
+      getRandomNumberInRange(-0.005, 0.005)
+    )
+  );
 
   useEffect(() => {
     const img = new Image();
@@ -28,18 +43,28 @@ export function Painting({ url, initialPosition }: PaintingProps) {
         getRandomNumberInDefaultRange()
       );
 
-  const rotation = new THREE.Euler(
-    getRandomNumberInRange(0, Math.PI * 2),
-    getRandomNumberInRange(0, Math.PI * 2),
-    getRandomNumberInRange(0, Math.PI * 2)
-  );
+  const rotation = initialRotation
+    ? initialRotation
+    : new THREE.Euler(
+        getRandomNumberInRange(0, Math.PI * 2),
+        getRandomNumberInRange(0, Math.PI * 2),
+        getRandomNumberInRange(0, Math.PI * 2)
+      );
+
+  useFrame(() => {
+    if (meshRef.current) {
+      meshRef.current.rotation.x += rotationDirection.x;
+      meshRef.current.rotation.y += rotationDirection.y;
+      meshRef.current.rotation.z += rotationDirection.z;
+    }
+  });
 
   return (
     <Suspense
       key={url + "-suspense"}
       fallback={<FallbackPaiting key={url + "-fallback"} position={position} />}
     >
-      <mesh position={position} name={url} rotation={rotation}>
+      <mesh ref={meshRef} position={position} name={url} rotation={rotation}>
         <boxGeometry args={[dimensions.width, dimensions.height, 0.1]} />
         <meshBasicMaterial attach="material-0" color="black" />
         <meshBasicMaterial attach="material-1" color="black" />
